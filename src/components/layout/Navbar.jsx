4ad5a93917d932b243c5c1315ced557bbaa82307
godriceeichie <emmaeichie@gmail.com>
2023-03-24 09:02:29 +0100

    Changed the colorof the hamburger toggle button

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = ({ url }) => {
     const linkColor = {color: (url !== '') ? 'black' : 'white'}
     const navbarStyle = {
         background: (url !== '') ? 'white' : 'linear-gradient(90deg, rgba(31,70,127,1) 0%, rgba(30,70,129,1) 33%, rgba(31,72,128,1) 67%)',
-        boxShadow: (url !== '') ? '0px 5px 25px -3px rgba(0,0,0,0.31)' : 'none'
+        boxShadow: (url !== '') ? '0px 2px 25px -3px rgba(0,0,0,0.31)' : 'none'
     }
     
     return (
