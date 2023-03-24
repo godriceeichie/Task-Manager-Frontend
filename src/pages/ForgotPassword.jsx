0c8cb3cc3e909dafe11cd35f5ca334312a57e776
godriceeichie <emmaeichie@gmail.com>
2023-03-24 12:41:01 +0100

    Finished creating the forgot password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Input } from '@mantine/core';
+import { Input, Button, rem } from '@mantine/core';
 import { AiOutlineMail } from 'react-icons/ai';
 
 const ForgotPassword = () => {
     return (
         <div className="forgot-password-wrapper">
             <form action="" method="post" className="forgot-password">
-                <h1>Forgot Password</h1>
-                <Input.Wrapper label="Email">
+                <h1 className="forgot-password-heading">Forgot Password</h1>
+                <Input.Wrapper>
                     <Input
                         icon={<AiOutlineMail />}
                         label="Password"
@@ -16,6 +16,15 @@ const ForgotPassword = () => {
                         size="md"
                     />
                 </Input.Wrapper>
+                <Button color="green" styles={(theme) => ({root: {
+                        width: rem(200),
+                        height: rem(50),
+                        fontSize: rem(19),
+                        alignSelf: 'center',
+                        marginTop: 20
+                    }})}>
+                       Reset Password
+                </Button>
             </form>
         </div>
     );
